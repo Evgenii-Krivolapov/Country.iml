e1224acdb2baa8ef8e3f1a69254c13705afd566d
Evgenii Krivolapov <leonidovich.w@gmail.com>
2022-07-22 17:07:46 +0300

    Работа с приватными переменными, создали для них setter and getter, а так же конструктор.

---
+++ src/Country.java
@@ -0,0 +1,51 @@
+public class Country {
+    private String russia;
+    private double amountPeople = 144.1;
+    private double square = 17.1;
+    private String moscow;
+    private String blackSea;
+
+    public Country(String russia) {
+        this.russia = russia;
+    }
+
+    public String getRussia() {
+        return russia;
+    }
+
+    public double getAmountPeople() {
+        return amountPeople;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public String getMoscow() {
+        return moscow;
+    }
+
+    public String getBlackSea() {
+        return blackSea;
+    }
+
+    public void setRussia() {
+        this.russia = russia;
+    }
+
+    public void setAmountPeople() {
+        this.amountPeople = amountPeople;
+    }
+
+    public void setSquare() {
+        this.square = square;
+    }
+
+    public void setMoscow() {
+        this.moscow = moscow;
+    }
+
+    public void setBlackSea() {
+        this.blackSea = blackSea;
+    }
+}
